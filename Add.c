0f7d9b406c66b4c56255617e74efefe200d86806
Bhavyacodes117 <bhavyachaudhary117@gmail.com>
2026-08-12 15:54:46 +0530

    Add files via upload

---
+++ Add.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,c;
+    printf("Enter the 1st No:-");
+    scanf("%d",&a);
+    printf("Enter the 2nd No:-");
+    scanf("%d",&b);
+
+    c=a+b;
+    printf("The result of addition is %d",c);
+}
---
+++ Hr to min.c
@@ -0,0 +1,9 @@
+#include<Stdio.h>
+void main()
+{
+int hr, min;
+printf("Enter the no. of hours\n");
+scanf("%d",&hr);
+min=60*hr;
+printf("%d hours=%d minutes\n",hr,min);
+}
